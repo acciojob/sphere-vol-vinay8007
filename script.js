f5c5a09029717c77badee9b678aba774c5d54d47
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2025-09-30 19:36:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function volume_sphere() {
         document.getElementById("volume").value = "NaN";
         return false;
     }
+	if (isNaN(radius) || radius < 0) {
+       document.getElementById("volume").value = "";
+       return false;
+   }
 
     // Formula: (4/3) * π * r^3
     let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
